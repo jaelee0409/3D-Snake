2df2b072af5dcf8326a01fafe069d75a190764d3
U-W24696\CKIRUser <jaeslee@utexas.edu>
2024-12-14 20:27:22 +0900

    Add Lazy Foo's color modulation

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,342 @@
-//Using SDL and standard IO
-#include <SDL.h>
-#include <stdio.h>
+/*This source code copyrighted by Lazy Foo' Productions 2004-2024
+and may not be redistributed without written permission.*/
 
-//Screen dimension constants
+// Using SDL, SDL_image, standard IO, and strings
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+// Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
+//Texture wrapper class
+class LTexture
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+    public:
+        //Initializes variables
+        LTexture();
 
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+        //Deallocates memory
+        ~LTexture();
+
+        //Loads image at specified path
+        bool loadFromFile( std::string path );
+
+        //Deallocates texture
+        void free();
+
+        //Set color modulation
+        void setColor( Uint8 red, Uint8 green, Uint8 blue );
+
+        //Renders texture at given point
+        void render( int x, int y, SDL_Rect* clip = NULL );
+
+        //Gets image dimensions
+        int getWidth();
+        int getHeight();
+
+    private:
+        //The actual hardware texture
+        SDL_Texture* mTexture;
+
+        //Image dimensions
+        int mWidth;
+        int mHeight;
+};
+
+// Starts up SDL and creates window
+bool init();
+
+// Loads media
+bool loadMedia();
+
+// Frees media and shuts down SDL
+void close();
+
+// The window we'll be rendering to
+SDL_Window *gWindow = NULL;
+
+// The window renderer
+SDL_Renderer *gRenderer = NULL;
+
+//Scene texture
+LTexture gModulatedTexture;
+
+LTexture::LTexture()
+{
+    // Initialize
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+LTexture::~LTexture()
+{
+    // Deallocate
+    free();
+}
+
+bool LTexture::loadFromFile(std::string path)
+{
+    // Get rid of preexisting texture
+    free();
+
+    // The final texture
+    SDL_Texture *newTexture = NULL;
+
+    // Load image at specified path
+    SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+    if (loadedSurface == NULL)
     {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
     }
     else
     {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
+        // Color key image
+        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+        // Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+        if (newTexture == NULL)
         {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
         }
         else
         {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+            // Get image dimensions
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
         }
-    }
-    
-    //Destroy window
-    SDL_DestroyWindow( window );
 
-    //Quit SDL subsystems
+        // Get rid of old loaded surface
+        SDL_FreeSurface(loadedSurface);
+    }
+
+    // Return success
+    mTexture = newTexture;
+    return mTexture != NULL;
+}
+
+void LTexture::free()
+{
+    // Free texture if it exists
+    if (mTexture != NULL)
+    {
+        SDL_DestroyTexture(mTexture);
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
+
+void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
+{
+    //Modulate texture
+    SDL_SetTextureColorMod( mTexture, red, green, blue );
+}
+
+void LTexture::render(int x, int y, SDL_Rect *clip)
+{
+    // Set rendering space and render to screen
+    SDL_Rect renderQuad = {x, y, mWidth, mHeight};
+
+    // Set clip rendering dimensions
+    if (clip != NULL)
+    {
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
+
+    // Render to screen
+    SDL_RenderCopy(gRenderer, mTexture, clip, &renderQuad);
+}
+
+int LTexture::getWidth()
+{
+    return mWidth;
+}
+
+int LTexture::getHeight()
+{
+    return mHeight;
+}
+
+bool init()
+{
+    bool success = true;
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        return false; // Early return on failure
+    }
+
+    // Set texture filtering to linear
+    if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+    {
+        printf("Warning: Linear texture filtering not enabled!\n");
+    }
+
+    // Create window
+    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (gWindow == NULL)
+    {
+        printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+        return false; // Early return on failure
+    }
+
+    // Create renderer for window
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (gRenderer == NULL)
+    {
+        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+        return false; // Early return on failure
+    }
+
+    // Initialize renderer color
+    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    // Initialize PNG loading
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags))
+    {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return false; // Early return on failure
+    }
+
+    return success; // Return success if no errors occurred
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load texture
+	if( !gModulatedTexture.loadFromFile( "../assets/colors.png" ) )
+	{
+		printf( "Failed to load colors texture!\n" );
+		success = false;
+	}
+	
+	return success;
+}
+
+void close()
+{
+    // Free loaded images
+    gModulatedTexture.free();
+
+    // Destroy window
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    gRenderer = NULL;
+
+    // Quit SDL subsystems
+    IMG_Quit();
     SDL_Quit();
+}
+
+// Handle all rendering logic
+void render(Uint8 r, Uint8 g, Uint8 b)
+{
+    //Clear screen
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+    SDL_RenderClear( gRenderer );
+
+    //Modulate and render texture
+    gModulatedTexture.setColor( r, g, b );
+    gModulatedTexture.render( 0, 0 );
+
+    //Update screen
+    SDL_RenderPresent( gRenderer );
+}
+
+int main(int argc, char *args[])
+{
+    // Start up SDL and create window
+    if (!init())
+    {
+        printf("Failed to initialize!\n");
+        return -1; // Early return on failure
+    }
+
+    // Load media
+    if (!loadMedia())
+    {
+        printf("Failed to load media!\n");
+        close();   // Ensure cleanup on failure
+        return -1; // Early return on failure
+    }
+
+    //Main loop flag
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+
+    //Modulation components
+    Uint8 r = 255;
+    Uint8 g = 255;
+    Uint8 b = 255;
+
+    // While application is running
+    while (!quit)
+    {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+            //On keypress change rgb values
+            else if( e.type == SDL_KEYDOWN )
+            {
+                switch( e.key.keysym.sym )
+                {
+                    //Increase red
+                    case SDLK_q:
+                    r += 32;
+                    break;
+                    
+                    //Increase green
+                    case SDLK_w:
+                    g += 32;
+                    break;
+                    
+                    //Increase blue
+                    case SDLK_e:
+                    b += 32;
+                    break;
+                    
+                    //Decrease red
+                    case SDLK_a:
+                    r -= 32;
+                    break;
+                    
+                    //Decrease green
+                    case SDLK_s:
+                    g -= 32;
+                    break;
+                    
+                    //Decrease blue
+                    case SDLK_d:
+                    b -= 32;
+                    break;
+                }
+            }
+        }
+
+        render(r, g, b); // Handle all rendering in a separate function
+    }
+
+    // Free resources and close SDL
+    close();
 
     return 0;
 }
